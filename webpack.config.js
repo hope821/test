8e91a2b440da9de45e1bfd36e6dddd5dbf049eaa
johnny <johnny.kjh@kakaocorp.com>
2019-04-02 10:22:21 +0900

    babel option

--- webpack.config.js
+++ webpack.config.js
@@ -18,7 +18,11 @@ module.exports = {
                 test: /\.(js|jsx)$/,
                 exclude: /node_modules/,
                 use: {
-                    loader: "babel-loader"
+                    loader: "babel-loader",
+                    options : {
+                        presets : [ '@babel/preset-env', '@babel/preset-react' ],
+                        plugins : [ '@babel/plugin-proposal-class-properties' ]
+                    }
                 }
             },
             {
